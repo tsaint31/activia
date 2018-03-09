b432a62d12831fc88994a45da35321f4e3083521
thierry saintmaxent <thierry.saintmaxent@decathlon.com>
2018-03-09 09:58:23 +0100

    dev

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to ACTIVIA</h1>
+          <h1 className="App-title">Welcome to ACTIVIA!</h1>
         </header>
         <p className="App-intro">
           To get started, enter your price and follow the evolution.
